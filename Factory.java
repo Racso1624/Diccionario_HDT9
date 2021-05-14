714619857b9f8d49bf7fd9a8a82898c1168537cf
Racso1624 <fer10_lb@outlook.com>
2021-05-14 13:59:15 -0600

    Se arregla error de BST

--- Factory.java
+++ Factory.java
@@ -6,7 +6,7 @@ public class Factory {
     public MapGeneral factory(int opcion) {
 
         if (opcion == 1) {
-            return new RedBlackTree();
+            return new RedBlackBST<>();
         } else if (opcion == 2) {
             return new hashMap();
         } else {
